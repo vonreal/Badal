15ef667f8c5e007471f432cac466ec34d379cdac
jiun <vonreal20@gmail.com>
2022-07-10 15:27:50 +0900

    ✅ melon music clone

---
+++ Badal/melonMusicViewController.swift
@@ -0,0 +1,37 @@
+//
+//  melonMusicViewController.swift
+//  Badal
+//
+//  Created by 나지운 on 2022/07/10.
+//
+
+import UIKit
+
+class melonMusicViewController: UIViewController {
+
+    @IBOutlet weak var titleButton: UIButton!
+    
+    @IBOutlet weak var singerLabel: UILabel!
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+    @IBOutlet weak var recommedButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleButton.setTitle("strawberry moon", for: .normal)
+        singerLabel.text = "아이유"
+        albumImageView.layer.cornerRadius = 5
+        
+        designButton(recommedButton)
+
+    }
+    
+    func designButton(_ button: UIButton) {
+        button.setTitle("  유사곡  ", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.lightGray.cgColor
+    }
+    
+}
